ea35c04aa66579e620800cc45c0a978994769d7c
jabanto22 <jean.abanto@gmail.com>
2021-04-23 08:56:33 +0800

    Simple tweet harvester to json file

---
+++ tweepy-harvester/harvester/config.py
@@ -0,0 +1,45 @@
+# tweepy-bots/bots/config.py
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+
+
+# CouchDB credentials
+# NODE_ID = os.getenv("NODE_ID")
+# COUCHDB_USER = os.getenv("COUCHDB_USER")
+# COUCHDB_PASSWORD = os.getenv("COUCHDB_PASSWORD")
+
+# Twitter credentials
+TWITTER_CONSUMER_KEY = os.getenv("TWITTER_CONSUMER_KEY")
+TWITTER_CONSUMER_SECRET = os.getenv("TWITTER_CONSUMER_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+# Change value to filter out tweets intended for analysis
+# FILTER = [140.928224, -39.449811, 150.024903, -33.959390] # filter tweets from VICTORIA
+FILTER = [144.593742, -38.433859, 145.512529,-37.511274] # filter tweets from AUSTRALIA
+
+def create_api():
+    consumer_key = TWITTER_CONSUMER_KEY
+    consumer_secret = TWITTER_CONSUMER_SECRET
+    access_token = TWITTER_ACCESS_TOKEN
+    access_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+
+    try:
+        api.verify_credentials()
+
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+
+    logger.info("API created")
+
+    return api
---
+++ tweepy-harvester/harvester/stream_tweet.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# tweepy-bots/bots/tweet_streamer.py
+
+from __future__ import absolute_import, print_function
+
+from tweepy import OAuthHandler, Stream, StreamListener
+import logging
+from config import create_api, FILTER
+import json
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+class DBStreamListener(StreamListener):
+    """ 
+    A listener handles tweets that are received from the stream.
+    This is a basic listener that dumps received tweets to json file.
+    """
+    
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+        
+        self.save_file = open('twitter.json','a')
+        
+        super().__init__()
+
+    def on_status(self, tweet):
+        logger.info(f"Processing tweet id {tweet.id}")
+        
+    def on_data(self, tweet):   
+        self.save_file.write(str(tweet).strip("\n"))
+
+    def on_error(self, status):
+        logger.error(status)
+
+def main():
+    api = create_api()
+    stream_listener = DBStreamListener(api)
+    stream = Stream(api.auth, stream_listener)
+    stream.filter(locations=FILTER)
+
+if __name__ == "__main__":
+    main()
