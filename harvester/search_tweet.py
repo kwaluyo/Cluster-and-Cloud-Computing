08efe060ac9363e83812a0853863a679e0e07642
jabanto22 <jean.abanto@gmail.com>
2021-05-02 11:42:17 +0800

    enclose search in infinite loop

--- harvester/search_tweet.py
+++ harvester/search_tweet.py
@@ -4,7 +4,7 @@ import threading
 import logging
 import json
 import os
-import datetime
+import time, datetime
 
 from utils import load_config
 
@@ -25,20 +25,24 @@ def search_city(api, location, city, since_id):
     save_file = open(path, 'a', encoding="utf8")
 
     # Scrape tweets from January 2020 or the past 7 days
-    try:
-        for i in range(250):
-            # Returns tweets by users located within a given radius of the given latitude/longitude. 
-            # The location is preferentially taking from the Geotagging API, but will fall back to their Twitter profile.
-            # There are limits to the number of Tweets which can be accessed through the API. 
-            # If the limit of Tweets has occurred since the since_id, the since_id will be forced to the oldest ID available.
-            for tweet in api.search(lang=["en"], geocode=location, count=100, since_id=since_id):
+    while True:
+        try:
+            for i in range(250):
+                # Returns tweets by users located within a given radius of the given latitude/longitude. 
+                # The location is preferentially taking from the Geotagging API, but will fall back to their Twitter profile.
+                # There are limits to the number of Tweets which can be accessed through the API. 
+                # If the limit of Tweets has occurred since the since_id, the since_id will be forced to the oldest ID available.
+                for tweet in api.search(lang=["en"], geocode=location, count=100, since_id=since_id):
 
-                # Look for tweet
-                logger.info(f"Search: Processing tweet id {tweet.id}")
-                save_file.write("["+json.dumps(tweet._json, separators=(',', ':'))+"]\n")
+                    # Look for tweet
+                    logger.info(f"Search: Processing tweet id {tweet.id}")
+                    save_file.write("["+json.dumps(tweet._json, separators=(',', ':'))+"]\n")
 
-    except Exception as e:
-        logger.error("Tweet search error:", e)
+        except Exception as e:
+            logger.error("Tweet search error:", e)
+            continue
+
+        time.sleep(15*60 + 1)
 
 
 def main(api, config):
