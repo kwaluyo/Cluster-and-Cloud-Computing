ac5f2ee4ddf33fd65a052c837734ea49ee42516e
jabanto22 <jean.abanto@gmail.com>
2021-05-18 13:12:42 +0800

    create deploy instance shell script

--- Ansible/run-nectar.sh
+++ Ansible/run-nectar.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2021-grp-17-openrc.sh; ansible-playbook deploy_instances.yaml
+. ./unimelb-comp90024-2021-grp-17-openrc.sh; 
+
+echo "Please enter job number: "
+echo "1. Create instances"
+echo "2. Configure instances"
+echo "3. Deploy CouchDB cluster and Twitter harvester"
+
+read inpt
+
+case $inpt in 
+    "1")
+        ansible-playbook deploy_instances.yaml;
+        ;;
+    "2")
+        ansible-playbook config_instances.yaml -i inventory/hosts.ini
+        ;;
+    "3")
+        ansible-playbook deploy_harvester.yaml -i inventory/hosts.ini
+        ;;
+    *)
+        echo "Invalid job number."
+        ;;
+esac
+
