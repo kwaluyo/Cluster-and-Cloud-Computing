3dd0a2dc42d5735ed5d09a9c2482f874650799c8
Radhimas Djan <radhimas.djan@gmail.com>
2021-05-25 04:58:19 +1000

    change description

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,17 +6,16 @@ function Home(){
 
     return(
         <div className="content">
-            <div className="content1"> Project Title </div>
+            <div className="content1"> Find the happiest city</div>
             <div className="subcontent">
                 <div className="content2"> 
-                    <h4>Group x</h4>
-                    <h6>Member 1: Student id 1</h6>
-                    <h6>Member 2: Student id 2</h6>
-                    <h6>Member 3: Student id 3</h6>
-                    <h6>Member 4: Student id 4</h6>
-                    
+                <h4>Group 17</h4>
+                    <h6>Jeanelle Abanto: 1133815</h6>
+                    <h6>Kartika Waluyo: 1000555</h6>
+                    <h6>Radhimas Djan: 1146240</h6>
+                    <h6>Zi Jin: 987771</h6>                    
                 </div>
-                <div className="content3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer vitae justo eget magna fermentum iaculis eu non diam. At lectus urna duis convallis convallis tellus id interdum. Fermentum iaculis eu non diam. Ullamcorper eget nulla facilisi etiam. Urna et pharetra pharetra massa massa ultricies. Tristique et egestas quis ipsum. Sed odio morbi quis commodo odio. Venenatis cras sed felis eget velit aliquet sagittis. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Gravida cum sociis natoque penatibus. Etiam dignissim diam quis enim. </div>
+                <div className="content3"> The project is to explore the happiness of 4 cities in australia, based on their sentiment we will compare them with the aurin data that can be correlated we will use a 2014~2018 data for comparing with the aurin while we will be using a harvester to get a 2021 data and analyze their sentiment</div>                {/* <div className="content4"> content4 </div>
                 {/* <div className="content4"> content4 </div>
                 <div className="content5"> content5 </div> */}
             </div>
