a488ee0d75290f4b31a2726a20dde8c7c3320325
r4dhiDj <radhimas.djan@gmail.com>
2021-05-24 17:50:44 +0700

    add satisfaction

--- backend/views.py
+++ backend/views.py
@@ -36,6 +36,19 @@ def create_sentiment_view(db):
     # if not view.get_doc(db):
     view.sync(db)
 
+def create_sentiment_satisfaction_view(db):
+    view = design.ViewDefinition('data', 'rekap', """
+        function (doc) {
+            emit(doc.location, {compound:doc.sentiment.compound,negative:doc.sentiment.neg,
+                neutral:doc.sentiment.neu,positive:doc.sentiment.pos
+            },true);
+        }
+        """, '''\
+        _sum''')
+    # if not view.get_doc(db):
+    view.sync(db)
+    
 create_income_view(connectDB.dbIncome)
 create_sentiment_view(connectDB.dbSentiment)
 create_unemployment_view(connectDB.dbUnemployment)
+create_sentiment_satisfaction_view(connectDB.dbSentiment)
