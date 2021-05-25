89a0a49de399e347eb964d6da371020f93e00f85
kwaluyo <61862757+kwaluyo@users.noreply.github.com>
2021-05-26 02:47:16 +1000

    fix home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,12 +2,18 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 import "../App.css"
 
+
 function Home(){
 
     return(
         <div className="content">
-            <div className="content1"> Find the happiest city</div>
-            <div className="subcontent">
+            <div className="subcontent1">
+                    <div className="content1"> Happiest</div>
+                    <div className="content1-1"> City</div>
+            </div>
+     
+
+            <div className="subcontent2">
                 <div className="content2"> 
                 <h4>Group 17</h4>
                     <h6>Jeanelle Abanto: 1133815</h6>
@@ -15,7 +21,7 @@ function Home(){
                     <h6>Radhimas Djan: 1146240</h6>
                     <h6>Zi Jin: 987771</h6>                    
                 </div>
-                <div className="content3"> The project is to explore the happiness of 4 cities in australia, based on their sentiment we will compare them with the aurin data that can be correlated we will use a 2014~2018 data for comparing with the aurin while we will be using a harvester to get a 2021 data and analyze their sentiment</div>                {/* <div className="content4"> content4 </div>
+                <div className="content3"> Exploring the happiness of 4 big cities' residents in Australia, based on the sentiment analysis from tweets. The tweets from each city are then correlated with some aurin data from 2014 to 2018. Making use of a harvester, 2021 data are collected and their sentiments are analysed.</div>                {/* <div className="content4"> content4 </div>
                 {/* <div className="content4"> content4 </div>
                 <div className="content5"> content5 </div> */}
             </div>
